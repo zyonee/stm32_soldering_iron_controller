fc44cdc5f292414935d987aecc8d836a6e237edf
Björn Hutmacher <bjorn.hutmacher@gmail.com>
2021-09-12 20:29:26 +0200

    Personal threshold for tick filtering.

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -28,6 +28,7 @@ extern "C" {
 #endif
 
 /* Includes ------------------------------------------------------------------*/
+#include "stm32f1xx_hal.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -78,6 +79,8 @@ extern struct mallinfo mi;
 #endif
 /* USER CODE END EM */
 
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
+
 /* Exported functions prototypes ---------------------------------------------*/
 void Error_Handler(void);
 
@@ -110,6 +113,40 @@ extern uint32_t max_allocated;
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define EE_SCL_Pin GPIO_PIN_0
+#define EE_SCL_GPIO_Port GPIOA
+#define EE_SDA_Pin GPIO_PIN_1
+#define EE_SDA_GPIO_Port GPIOA
+#define TIP_Pin GPIO_PIN_2
+#define TIP_GPIO_Port GPIOA
+#define BUZ0_Pin GPIO_PIN_3
+#define BUZ0_GPIO_Port GPIOA
+#define BUZ1_Pin GPIO_PIN_4
+#define BUZ1_GPIO_Port GPIOA
+#define BUZ2_Pin GPIO_PIN_5
+#define BUZ2_GPIO_Port GPIOA
+#define WAKE_Pin GPIO_PIN_6
+#define WAKE_GPIO_Port GPIOA
+#define NTC_Pin GPIO_PIN_7
+#define NTC_GPIO_Port GPIOA
+#define PWM_Pin GPIO_PIN_0
+#define PWM_GPIO_Port GPIOB
+#define VIN_Pin GPIO_PIN_1
+#define VIN_GPIO_Port GPIOB
+#define HW_SCL_Pin GPIO_PIN_10
+#define HW_SCL_GPIO_Port GPIOB
+#define HW_SDA_Pin GPIO_PIN_11
+#define HW_SDA_GPIO_Port GPIOB
+#define SW_SDA_Pin GPIO_PIN_12
+#define SW_SDA_GPIO_Port GPIOB
+#define SW_SCL_Pin GPIO_PIN_13
+#define SW_SCL_GPIO_Port GPIOB
+#define ENC_SW_Pin GPIO_PIN_15
+#define ENC_SW_GPIO_Port GPIOA
+#define ENC_R_Pin GPIO_PIN_3
+#define ENC_R_GPIO_Port GPIOB
+#define ENC_L_Pin GPIO_PIN_4
+#define ENC_L_GPIO_Port GPIOB
 /* USER CODE BEGIN Private defines */
 #define WAKE_input()		HAL_GPIO_ReadPin(WAKE_GPIO_Port, WAKE_Pin)
 #define BUTTON_input()		HAL_GPIO_ReadPin(ENC_SW_GPIO_Port, ENC_SW_Pin)
